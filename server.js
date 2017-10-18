ecf1b8b14b3deba371560fab0834ec60017b54c0
Sanju Choudhary <mail2sanju@gmail.com>
2017-10-18 10:39:41 +0530

    Added protfolio page

--- server.js
+++ server.js
@@ -66,6 +66,14 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  // res.send('About Page');
+  res.render('projects.hbs', {
+    pageTitle: 'Protfolio Page',
+    //currentYear: new Date().getFullYear()
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
